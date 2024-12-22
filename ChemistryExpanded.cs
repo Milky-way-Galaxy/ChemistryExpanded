1d7ba3ef0a36e7c933620ef6dff3a7a7fd2465ac
Galaxy <157143295+Milky-way-Galaxy@users.noreply.github.com>
2024-12-22 22:22:52 +0700

    Update ChemistryExpanded.cs

--- ChemistryExpanded.cs
+++ ChemistryExpanded.cs
@@ -1,52 +1,112 @@
-using BepInEx;
-using CommonAPI;
 using System;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using xiaoye97;
 
-namespace ChemistryExpanded
+namespace WaterElectrolysis
 {
-    [BepInPlugin("com.milkyway.chemistryexpanded", "Chemistry Expanded", "1.0.0")]
-    public class ChemistryExpandedPlugin : BaseUnityPlugin
+    [BepInDependency("me.xiaoye97.plugin.Dyson.LDBTool", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("Gnimaerd.DSP.plugin.WaterElectrolysis", "WaterElectrolysis", "1.2")]
+    public class WaterElectrolysis : BaseUnityPlugin
     {
-        public void Awake()
-        {
-            Logger.LogInfo("Chemistry Expanded Mod Loaded");
+        private Sprite icon;
 
-            // Add the electrolysis recipe when the mod is loaded
-            AddElectrolysisRecipe();
+        void Start()
+        {
+            var ab = AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("Water_electrolysis.waterelecicon"));
+            icon = ab.LoadAsset<Sprite>("WaterElec3");
+
+            LDBTool.PreAddDataAction += AddTranslate;
+            LDBTool.PostAddDataAction += AddWaterToH;
         }
 
-        private void AddElectrolysisRecipe()
+        void AddWaterToH()
         {
-            // Creating a new recipe for Water Electrolysis
-            RecipeProto electrolysisRecipe = new RecipeProto
+            var ori = LDB.recipes.Select(23);
+
+            var waterele = ori.Copy();
+            waterele.ID = 443;
+            waterele.Explicit = true;
+            waterele.Name = "催化电解";
+            waterele.name = "催化电解".Translate();
+            waterele.Items = new int[] { 1000 };
+            waterele.ItemCounts = new int[] { 1 };
+            waterele.Results = new int[] { 1120 };
+            waterele.ResultCounts = new int[] { 1 };
+            waterele.GridIndex = 1110;
+            waterele.SID = "1110";
+            waterele.sid = "1110".Translate();
+            Traverse.Create(waterele).Field("_iconSprite").SetValue(icon);
+            waterele.TimeSpend = 30;
+            waterele.Description = "催化电解描述";
+            waterele.description = "催化电解描述".Translate();
+            waterele.preTech = LDB.techs.Select(1121);
+
+            // Add the recipe to the hydrogen item
+            var h = LDB.items.Select(1120);
+            h.recipes.Add(waterele);
+
+            LDBTool.PostAddProto(ProtoType.Recipe, waterele);
+        }
+
+        void AddTranslate()
+        {
+            var recipeName = new CustomStringProto(
+                id: 28001,
+                name: "催化电解",
+                zhcn: "催化电解",
+                enus: "Water Electrolysis",
+                frfr: "Water Electrolysis"
+            );
+
+            var desc = new CustomStringProto(
+                id: 28002,
+                name: "催化电解描述",
+                zhcn: "电解水并获取氢气。",
+                enus: "Electrolysis of water to produce hydrogen.",
+                frfr: "Electrolysis of water to produce hydrogen."
+            );
+
+            LDBTool.PreAddProto(ProtoType.String, recipeName);
+            LDBTool.PreAddProto(ProtoType.String, desc);
+        }
+
+        public class CustomStringProto : Proto
+        {
+            public string ZHCN; // Chinese translation
+            public string ENUS; // English translation
+            public string FRFR; // French translation
+
+            public CustomStringProto(int id, string name, string zhcn, string enus, string frfr)
             {
-                Name = "Water Electrolysis",  // Recipe name
-                Id = 1001,  // Recipe ID (you can choose a free ID in your game)
-                Time = 1f,  // 1 second for the electrolysis reaction
-                Icon = IconManager.GetIcon("icon"),  // Set the recipe icon (from the file named "icon.png")
-                Description = "Electrolyze water to obtain hydrogen.", // Description
-                // Inputs and outputs of the recipe
-                Ingredients = new RecipeIngredient[]
+                ID = id;
+                Name = name;
+                ZHCN = zhcn;
+                ENUS = enus;
+                FRFR = frfr;
+            }
+
+            public string GetTranslation(string languageCode)
+            {
+                if (languageCode == "ZHCN")
                 {
-                    new RecipeIngredient { ItemId = 1000, Amount = 1 }  // 1 water (ID of water in the game)
-                },
-                Products = new RecipeProduct[]
-                {
-                    new RecipeProduct { ItemId = 1001, Amount = 1 }  // 1 hydrogen (ID of hydrogen in the game)
+                    return ZHCN;
                 }
-            };
-
-            // Add the recipe to the game
-            LDBTool.AddRecipe(electrolysisRecipe);
-
-            // Here, you would typically set the energy value of hydrogen
-            // For simplicity, we'll set a static energy value for now.
-            float hydrogenEnergyValue = 440000f;  // Energy value in Joules (for example, 440kJ = 440,000 J)
-
-            // Calculate energy produced (for now just for simplicity as fixed value)
-            // The energy calculation can be placed in a more dynamic way depending on the game mechanics.
-            float producedEnergy = hydrogenEnergyValue;  // Energy produced per hydrogen (in Joules)
-            Logger.LogInfo($"Energy produced per hydrogen: {producedEnergy} Joules");
+                else if (languageCode == "ENUS")
+                {
+                    return ENUS;
+                }
+                else if (languageCode == "FRFR")
+                {
+                    return FRFR;
+                }
+                else
+                {
+                    return Name; // Default to the Name if the language is unsupported
+                }
+            }
         }
     }
-}
+}   
